4fe61fdc3c6f2d8e6ace7693e664ac81f9f8b5cd
Botondepanico <sebastian.quiroz@telefonica.com>
2025-03-17 16:30:25 -0300

    Actualización de archivos: index.html, script.js, manifest.json, boton de panico.png

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 document.getElementById('panicButton').addEventListener('click', () => {
   // URL del archivo MP3 en Google Drive (debe ser un enlace directo)
-  const mp3Url = 'https://drive.google.com/file/d/18UGHBVmZdTwAIF28XvNg_MlBQFZ6Ia2B/view?usp=sharing';
+  const mp3Url = 'https://drive.usercontent.google.com/u/0/uc?id=18UGHBVmZdTwAIF28XvNg_MlBQFZ6Ia2B&export=download';
 
   // Crear un elemento de audio y reproducir el sonido
   const audio = new Audio(mp3Url);
